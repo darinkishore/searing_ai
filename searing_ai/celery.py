c22895125d338aa90d2114de91dc0941b25df8cc
darinkishore <darinkishore@protonmail.com>
2022-10-09 17:28:06 -0400

    modified celery

--- searing_ai/celery.py
+++ searing_ai/celery.py
@@ -3,7 +3,7 @@ from celery import Celery
 from django.conf import settings
 
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'searing_ai.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'searing_ai.settings_do')
 
 app = Celery('searing_ai')
 
