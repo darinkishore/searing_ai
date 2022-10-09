48a6cd631b2151add447f5fcdded799774b1b6ac
darinkishore <darinkishore@protonmail.com>
2022-10-09 17:17:18 -0400

    added access_key and secret_key to storage_backends.py settings

--- apps/data/storage_backends.py
+++ apps/data/storage_backends.py
@@ -10,24 +10,26 @@ from storages.backends.s3boto3 import S3Boto3Storage, S3StaticStorage
 # add bucket-names to these!
 
 class PublicMediaStorage(S3Boto3Storage):
-    AWS_S3_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
-    AWS_S3_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
+
     location = settings.PUBLIC_MEDIA_LOCATION
 
     bucket_name = 'moshimedia'
     custom_domain = f'{bucket_name}.s3.amazonaws.com'
 
 
-class PrivateMediaStorage(S3Boto3Storage):
-    access_key = os.environ.get('AWS_ACCESS_KEY_ID')
-    secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
-
+class PrivateMediaStorage(S3Boto3Storage, ABC):
+    # override the access key and secret key
+    # default_acl = 'private'
     location = settings.PRIVATE_MEDIA_LOCATION
-
-    default_acl = 'private'
-
-    file_overwrite = False
     bucket_name = 'moshimedia'
     custom_domain = f'{bucket_name}.s3.amazonaws.com'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.access_key = os.environ.get('AWS_ACCESS_KEY_ID')
+        self.secret_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+
+    file_overwrite = False
+
 
 
