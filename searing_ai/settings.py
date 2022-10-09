1e8d6d89edffdad4a0ef225d6e1d52d5e7e54ecc
darinkishore <darinkishore@protonmail.com>
2022-10-09 16:34:40 -0400

    fixed setting for aws uplaoads

--- searing_ai/settings.py
+++ searing_ai/settings.py
@@ -261,7 +261,7 @@ if USE_SPACES:
 
     session = boto3.Session(
         aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-        aws_secret_access_key=os.environ.get('AWS_ACCESS_KEY_ID'))
+        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
 
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
